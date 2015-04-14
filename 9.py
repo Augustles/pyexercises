5539d2ff7726faa2390b22fddd423ca447db8336
augustles <1927064778@qq.com>
2015-04-14 20:18:52 +0800

    modify 9,common.py

--- 9.py
+++ 9.py
@@ -4,5 +4,4 @@ for i in range(1, 10):
   for j in range(1, i + 1):
       if i != j:
         print i,'*',j,'=',i*j
-		
 f.write("i,'*',j,'=',i*j")
--- common.py
+++ common.py
@@ -2,10 +2,8 @@
 import os
 import time
 import shutil
-today = time.strftime('%Y-%m-%d星期%w')#today
-now = time.strftime('%H:%M:%S') #打印now
+today = time.strftime('%Y/%m/%d %H:%M:%S week:%w')#today
 print today
-print now
 
 '''os.path.sep ：#路径分隔符 。以前老爱用'\' ，windows返回'/'。
 os.path.altsep：#（根目录，不是很确定，我用来做根目录。反正在windows表现是'/'）
