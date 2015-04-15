8095c76ea25d9cef7a6731d6c00f606e9ba9f052
augustles <1927064778@qq.com>
2015-04-15 17:59:11 +0800

    update 9.py

--- 9.py
+++ 9.py
@@ -1,14 +1,14 @@
 #coding=utf-8
 
-f = open('9.txt','w') # 打开文件
-f.write('9*9乘法口诀' + '\n') # 写入单行数据
+f = open('9.txt','w') # 打开文件,没有则创建
+f.write('9*9乘法口诀' + '\n') # 写入单行数据read,readline,readlines和write,writelines
 for i in range(1, 10):
     for j in range(1, i + 1):
         if i != j:
             data =  '%d * %d' %(j,i) + ' = ' + str(i*j) + '\n'
             f.writelines(data) # 逐行写入
             print data # 打印
-
+print f.seek()
 
 
 
