fdd307018af7beb69d6c70b260f7280ef4a0570c
augustles <1927064778@qq.com>
2015-04-15 09:30:18 +0800

    update pyerror.py

--- pyerror.py
+++ pyerror.py
@@ -9,7 +9,7 @@ def foo(s):
     n = int(s)
     logging.info('n >>> %d' %n) # logging的好处，它允许你指定记录信息的级别，有debug，info，warning，error等几个级别
     # print 'n >>> %d' % n # print就是有问题的变量打印,简单直接粗暴有效，出现ZeroDivisionError
-    # assert n != 0, 'n is zero!' # assert的意思是，表达式n != 0应该是True，否则，后面的代码就会AssertionError
+    # assert n != 0, 'n is zero!' # assert的意思是断言，表达式n != 0应该是True，否则，后面的代码就会AssertionError
     
     return 10 / n
 
