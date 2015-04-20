f4d45b6dbae6cea1a1cff3192a0a3686be7ff798
augustles <1927064778@qq.com>
2015-04-20 18:01:39 +0800

    update

--- now.py
+++ now.py
@@ -3,4 +3,12 @@
 from datetime import datetime
 
 now = datetime.now()
-print "%s:%s:%s,%s/%s/%s" % (now.hour,now.minute,now.second,now.day,now.month,now.year)
+day = str(int(now.day) - 1)
+file = r"C:\\mongo\\source\\Report_%s_%s_%s.xls" % (now.month, day, now.year)
+print file
+#now = datetime.now()
+# print "%s:%s:%s,%s/%s/%s" %
+# (now.hour,now.minute,now.second,now.day,now.month,now.year)
+
+# with open(r"C:\\mongo\\source\\report.xls","wb") as pf:
+#  pass
