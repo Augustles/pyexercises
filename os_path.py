e53cc157758a9dcb095869588d37a3476ce3b145
augustles <1927064778@qq.com>
2015-04-27 17:21:04 +0800

    os_path

---
+++ os_path.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+import os,datetime  
+
+  
+base_dir = r'd:\ttoo'  
+list = os.listdir(base_dir)  # 得到一个包含文件和文件夹的列表
+filelist = []
+for i in range(0, len(list)):  
+    path = os.path.join(base_dir, list[i]) # 返回一个完整路径
+    if os.path.isfile(path) == 1: # 判断是否为文件
+        mtime = os.path.getmtime(path) # 文件的修改时间
+        print mtime
+        date = datetime.datetime.fromtimestamp(mtime)
+        print list[i],' file最近修改时间是: ',date.strftime('%Y-%m-%d %H:%M:%S')
+    else:
+        mtime = os.path.getmtime(path) 
+        print mtime
+        date = datetime.datetime.fromtimestamp(mtime)
+        print list[i],' dir最近修改时间是: ',date.strftime('%Y-%m-%d %H:%M:%S')
+
