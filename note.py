72822b3e3e487cda56779f5ff9cf2827c7c9c3e9
augustles <1927064778@qq.com>
2015-04-30 11:24:35 +0800

    update

--- note.py
+++ note.py
@@ -8,7 +8,8 @@
 # 函数也是一个对象，任何一个有__call__()特殊方法的对象都被当作函数
 # 2)上下文管理器(content manager),语法为with ... as ...
 # 会自动关闭文件，实际上是with是定义了__enter__()和__exit__()特殊方法
-# 3)对象的属性，python对象可能有多个属性(attribute)
+# 3)对象的属性，python对象可能有多个属性(attribute),dir()获取对象所有的属性和方法
+# type获取对象的类型，isinstance判断class的类型
 # 4)闭包(closure)是函数式编程的重要的语法结构
 # 函数和对象的根本目的是以某种逻辑方式组织代码，并提高代码的可重复使用性(reusability)
 # 一个函数和环境变量合在一起，就构成了一个闭包(closure)
@@ -23,7 +24,19 @@
 
 
 # python__init__用来对你的对象进行一些初始化，__del__删除一个类的实例调用
-# python内置类属性__name__,__bases__,__dict__,__doc__,__module__
+# python中属性和方法区别，属性是变量，方法是函数
+# python内置类class属性__name__,__bases__,__dict__,__doc__,__module__
+# python内置模块module属性__doc__,__name__,__dict__,__file__
+# python内置实例instance属性__dict__,__class__
+# python内置函数和方法built-in functions and methods __doc__,__name__,__self__,*__module__
+# python def定义的函数__doc__,__name__,*__module__,*__dict__
+# python 方法method__doc__,__name__,*__module__
+# python 生成器generator__iter__,gi_code,gi_frame,gi_running,next,close,send,throw
+# 此外，还有代码块code,栈帧frame,追踪traceback
+# 使用inspct模块提供一系列函数用于帮助使用自省
+# 检查对象类型,ismodule(obj),is{class|function|method|builtin}(obj),isroutine(obj)是否可调用类型
+# 获取对象信息,getmembers(obj)dir,getmodule(obj),get{source|sourcelines}(obj)
+# getargspec(func)仅用于方法,getcallargs(func),getmro(cls)
 # python内置函数 abs(),divmod,input,open,staticmethod,all,enmumerate,int,ord,str
 # any,eval_r,isinstance,pow,sum,basestring,execfile,issubclass,print,super
 # bin,file,iter,property,tuple,bool,filter,len,range,type,bytearray,float,list,raw_input,unichr
