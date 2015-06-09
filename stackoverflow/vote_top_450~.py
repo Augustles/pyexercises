bb83b8e8d3709a727b37c3d5f506708b1bb42405
augustles <1927064778@qq.com>
2015-06-09 17:50:20 +0800

    update

---
+++ stackoverflow/vote_top_450~.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+# stackoverflow top 450~
+
+# 1.when is 'i += x' different from 'i = i + x' in python
+# link:http://stackoverflow.com/questions/15376509/when-is-i-x-different-from-i-i-x-in-python
+# 2.why is there no GIL in java virtual machine why does python need one so bad
+# link:http://stackoverflow.com/questions/991904/why-is-there-no-gil-in-the-java-virtual-machine-why-does-python-need-one-so-bad
+# 3.how to check if type of a variable is string in python
+# link:http://stackoverflow.com/questions/4843173/how-to-check-if-type-of-a-variable-is-string-in-python
+# 4.reading binary file in pythotn
+# link:http://stackoverflow.com/questions/1035340/reading-binary-file-in-python
+# 5.django-what is the difference between render(),render_to_response() and direct_to _template()
+# link:http://stackoverflow.com/questions/5154358/django-what-is-the-difference-between-render-render-to-response-and-direc
+# 6.python urlencode string
+# link:http://stackoverflow.com/questions/5607551/python-urlencode-string
+# 7.why use python's os module methods instead of execting shell commands directly
+# link:http://stackoverflow.com/questions/28572833/why-use-pythons-os-module-methods-instead-of-executing-shell-commands-directly
+# 8.get md5 hash of big files in python
+# link:http://stackoverflow.com/questions/1131220/get-md5-hash-of-big-files-in-python
+# 9.class method differeces in python:bound,unbound and static
+# link:http://stackoverflow.com/questions/114214/class-method-differences-in-python-bound-unbound-and-static
