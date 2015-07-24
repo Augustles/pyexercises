f801d9c550254e00e6b163ba7b9c3f7e3f39fe5a
augustles <1927064778@qq.com>
2015-07-24 15:22:16 +0800

    update

--- stardardLibrary/walk_path.py
+++ stardardLibrary/walk_path.py
@@ -21,6 +21,16 @@ def walk_path():
         x = os.path.join(path, i)
         if os.path.isfile(x):
             print x
+# 获取整个目录下文件
+def walk_file(path):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            with open('walk.txt', 'a') as f:
+                f.write(os.path.abspath(root+os.sep+file)+ '\n')
+# 获取当前目录文件
+def current():
+    for file in os.listdir('.'):
+        print(os.path.abspath(file))
 
 if __name__ == '__main__':
     print 'start walk_path ...'
