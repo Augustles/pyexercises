cb47669c478fe7a6ae92b135da341886588e59ec
augustles <1927064778@qq.com>
2015-08-14 18:00:23 +0800

    update

--- coroutine.py
+++ coroutine.py
@@ -3,33 +3,58 @@
 
 # 协程
 import time
+import re
+
 def consumer():
     r = ''
     while True:
+        # 接收produce的n值
         n = yield r
-        if not n:
-            return
+        # if not n:
+        #     return
         print('[CONSUMER] consuming %s...' % n)
         time.sleep(1)
         r = '200 OK'
+        x = 'hello'
 
 def produce(c):
-    c.next()
+    '''x.next() -> the next value'''
+    c.next() # next启动进入consumer的生成器
+    # links = []
+    # n = 0
+    # f = range(0,300,25)
+    # for n in f:
+    #     num = 'start='+ str(n)
+    #     url = re.sub(r'start\=\d+', num, doulists)
+    #     links.append(url)
     n = 0
     while n < 5:
         n = n + 1
         print('[PRODUCER] Producing %s...' % n)
+        # 切换到consumer执行,consumer完成后,继续执行produce
+        '''
+        Docstring:
+        send(arg) -> send 'arg' into generator,
+        return next yielded value or raise StopIteration.
+        '''
         r = c.send(n)
         print('[PRODUCER] Consumer return: %s' % r)
-    c.close()
-    print('hello')
+
+
+    c.close() # 关闭生成器
+
+doulists = 'http://www.douban.com/doulist/38849533/?start=50&sort=seq&sub_type='
 
 produce(consumer())
 
+# produce向consumer发送n值,
+# consumer向produce发送r值,相互交换
+# 先执行一段,然后在执行其他,再回来执行剩下代码
+# 不共享其变量
 # 注意到consumer函数是一个generator（生成器），把一个consumer传入produce后：
 
 # 1. 首先调用c.next()启动生成器；
-# 2. 然后，一旦生产了东西，通过c.send(n)切换到consumer执行；
+# 2. 然后，一旦生产了东西(n)，通过c.send(n)切换到consumer执行；
 # 3. consumer通过yield拿到消息，处理，又通过yield把结果传回；
 # 4. produce拿到consumer处理的结果，继续生产下一条消息；
 # 5. produce决定不生产了，通过c.close()关闭consumer，整个过程结束。
