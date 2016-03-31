4a067cfb70ea0bf50d03b134c12136cb521e979a
xiao kang <Augustles@users.noreply.github.com>
2016-03-31 21:44:37 +0800

    Create sqlalchemy_test.py

---
+++ thirdLibray/sqlalchemy_test.py
@@ -0,0 +1,12 @@
+##sqlalchemy
+
+####pip install sqlalchemy
+####https://www.keakon.net/2012/12/03/SQLAlchemy%E4%BD%BF%E7%94%A8%E7%BB%8F%E9%AA%8C
+* from sqlalchemy import create_engine
+* from sqlalchemy.orm import sessionmaker
+* DB_CONNECT_STRING = 'mysql+mysqldb://root:nana@localhost/s?charset=utf8'
+* engine = create_engine(DB_CONNECT_STRING, echo=True)
+* DB_Session = sessionmaker(bind=engine)
+* session = DB_Session()
+* session.execute('create database abc')
+* session.execute('show databases').fetchall()
