51730da404d47c15e47fa00e5ee8466a2133d487
August <August@Augusts-MacBook-Pro.local>
2016-05-15 22:40:07 +0800

    update

--- thirdLibray/cv2-test.py
+++ thirdLibray/cv2-test.py
@@ -26,32 +26,44 @@ im1 = cv2.imread('22.jpg', cv2.IMREAD_GRAYSCALE)  # 转为灰度
 
 # 二值化 (转成纯黑白)
 # 直接设定阈值, 为什么是140呢？试出来的，或者参考直方图
-im = im.point(lambda x: 0 if x > 140 else 255)  # 简单设定阈值
+im = im.point(lambda x: 255 if x > 140 else 0)  # 简单设定阈值
 im = im.convert('1')  # 转为黑白
 # 1. 普通二值化
 # 2. 自适应二值化
 im1 = cv2.adaptiveThreshold(
     im, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 40)
 im1 = cv2.resize(im1, (0, 0), fx=4, fy=4)  # 将图片放大4倍, 方便降噪和识别
-# 噪点
-im1 = cv2.medianBlur(im1, 9)  # 模糊降噪
+# 噪点 1, 2结合使用
+# 1. 8邻域降噪, 小的噪点
+# 2. 连通域降噪, 大的噪点
+
+im1 = cv2.medianBlur(im1, 9)  # 均值滤波(模糊降噪)
 im = im.filter(ImageFilter.MedianFilter())  # 中值滤波过滤(中值去噪)
-# 干扰线 flood fill
+# 维纳滤波
+# 干扰线
+# 1. cv2.HoughLines 检测直线
+# 2. 寻找连通线, http://blog.csdn.net/problc/article/details/5579475
+# flood fill(提取字符)
 h, w = im1.shape[:2]
 mask = np.zeros((h + 2, w + 2), np.uint8)
 cv2.floodFill(im1, mask, (0, 0), 255)
-
-# 膨胀算法
-# kernel = np.ones((2, 2), np.uint8)
-# erosion = cv2.erode(im1, kernel, iterations=1)
 
 # 腐蚀算法
 # kernel = np.ones((2, 2), np.uint8)
 # erosion = cv2.dilate(im1, kernel, iterations=1)
 
+# 膨胀算法
+# kernel = np.ones((2, 2), np.uint8)
+# erosion = cv2.erode(im1, kernel, iterations=1)
+
+
 # 补点
 # kernel = np.ones((1, 1), np.uint8)
 # cv2.morphologyEx(im1, cv2.MORPH_OPEN, kernel)
+
+# 标准化
+# 1. 旋转, cv2.warpAffine函数就是进行旋转操作
+# 2. 归一化
 
 # 分割
 # 1. 锤子投影
